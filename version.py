68b1bef92001da9de4bcc233cb3654064978dee9
yuriy812 <sitecadilac@mail.ru>
2024-06-21 22:54:24 +0300

    add_run

--- version.py
+++ version.py
@@ -1,8 +1,24 @@
 HELP = """
 help - напечатать справку по программе.
-add - добавить задачу в список (название задачи запрашив
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
 show - напечатать все добавленные задачи."""
 
-command = input("Введите команду: ")
+tasks = []
+
+run = True
+
+while run:
+    command = input("Введите команду: ")
 if command == "help":
     print(HELP)
+elif command == "show":
+    print(tasks)
+elif command == "add":
+    task = input("Введите название задачи: ")
+    tasks.append(task)
+    print("Задача добавлена")
+else:
+    print("Неизвестная команда")
+    break
+
+print("До свидания!")
